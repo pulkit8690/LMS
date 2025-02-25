94f1c1bd3da470e5dfc0d0299c09943c71b9ab6e
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2025-02-25 19:59:52 +0530

    commit

--- run_server.py
+++ run_server.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 5000)),
+        port=int(os.getenv("PORT", 10000)),
         debug=True,
         allow_unsafe_werkzeug=True,
     )
